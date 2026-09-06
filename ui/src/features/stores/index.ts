9dcffa7fe1fd99f9917fd0c956bd90f10e7852e6
kardelitaitu <adikaradwiatmaja@gmail.com>
2026-09-07 01:50:09 +0700

    chore(ui): drop stale features/stores deletion entries

--- ui/src/features/stores/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MultiStoreDashboardScreen } from './MultiStoreDashboardScreen';
-export { default as TopologyScreen } from './TopologyScreen';
-export { default as TerminalStatusPanel } from './TerminalStatusPanel';
